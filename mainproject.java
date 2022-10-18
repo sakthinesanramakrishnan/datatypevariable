96ababe408956f015f0dcc508644f51823aecc77
sakthinesanramakrishnan <116069022+sakthinesanramakrishnan@users.noreply.github.com>
2022-10-18 06:34:53 -0400

    Add files via upload

---
+++ mainproject.java
@@ -0,0 +1,33 @@
+package Com.edu;
+
+public class mainproject {
+
+	public static void main(String[] args) {
+		//decalaration of variables
+		int intval;
+		float fval;
+		double dval;
+		char cval;
+		short sval;
+		byte bval;
+		long lval;
+		boolean boolval;
+		
+		//assign the values
+		intval=789;
+		fval=456.2f;
+		dval=123.3;
+		cval='s';
+		lval=8723;
+		boolval=false;
+		
+		//displaying
+		System.out.println("Integer value="+intval);
+		System.out.println("Float value="+fval);
+		System.out.println("Double value="+dval);
+		System.out.println("Char value="+cval);
+		System.out.println("Long value="+lval);
+		System.out.println("Boolean value="+boolval);
+	}
+
+}
